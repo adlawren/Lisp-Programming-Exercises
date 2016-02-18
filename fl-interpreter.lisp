6d030d28e0d2992a3956c52eb85ab63e2732df14
HeRc010 <adlawren010@gmail.com>
2016-02-17 22:30:51 -0700

    Refactored the interpreter; all primitive helper functions removed and the associated is embedded inline within the body of the primary interpreter function.

--- fl-interpreter.lisp
+++ fl-interpreter.lisp
@@ -12,333 +12,171 @@
             first-item
             'if
           )
-          (fl-interp-if E P)
+          (if (equal T (fl-interp (cadr E) P))
+              (fl-interp (caddr E) P)
+              (fl-interp (cadddr E) P)
+          )
         )
         (
           (equal
             first-item
             'null
           )
-          (fl-interp-null E P)
+          (null (fl-interp (cadr E) P))
         )
         (
           (equal
             first-item
             'atom
           )
-          (fl-interp-atom E P)
+          (atom (fl-interp (cadr E) P))
         )
         (
           (equal
             first-item
             'eq
           )
-          (fl-interp-eq E P)
+          (eq
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             'first
           )
-          (fl-interp-first E P)
+          (car
+            (fl-interp (cadr E) P)
+          )
         )
         (
           (equal
             first-item
             'rest
           )
-          (fl-interp-rest E P)
+          (cdr
+            (fl-interp (cadr E) P)
+          )
         )
         (
           (equal
             first-item
             'cons
           )
-          (fl-interp-cons E P)
+          (cons (fl-interp (cadr E) P) (fl-interp (caddr E) P))
         )
         (
           (equal
             first-item
             'equal
           )
-          (fl-interp-equal E P)
+          (equal
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             'number
           )
-          (fl-interp-number E P)
+          (numberp (fl-interp (cadr E) P))
         )
         (
           (equal
             first-item
             '+
           )
-          (fl-interp-add E P)
+          (+
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             '-
           )
-          (fl-interp-sub E P)
+          (-
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             '*
           )
-          (fl-interp-mul E P)
+          (*
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             '>
           )
-          (fl-interp-greater-than E P)
+          (>
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             '<
           )
-          (fl-interp-less-than E P)
+          (<
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             '=
           )
-          (fl-interp-equals E P)
+          (=
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             'and
           )
-          (fl-interp-and E P)
+          (and
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             'or
           )
-          (fl-interp-or E P)
+          (or
+            (fl-interp (cadr E) P)
+            (fl-interp (caddr E) P)
+          )
         )
         (
           (equal
             first-item
             'not
           )
-          (fl-interp-not E P)
+          (not
+            (fl-interp (cadr E) P)
+          )
         )
         (T
           (fl-parse-user-defined-function E P (fl-get-function-definition E P))
         )
       )
     )
-  )
-)
-
-; ...
-
-; Helper function which computes the primitive if function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-if (E P)
-  (if (equal T (fl-interp (cadr E) P))
-      (fl-interp (caddr E) P)
-      (fl-interp (cadddr E) P)
-  )
-)
-
-; Helper function which computes the primitive null function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-null (E P)
-  (null (fl-interp (cadr E) P))
-)
-
-; Helper function which computes the primitive atom function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-atom (E P)
-  (atom (fl-interp (cadr E) P))
-)
-
-; Helper function which computes the primitive eq function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-eq (E P)
-  (eq
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive first function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-first (E P)
-  (car
-    (fl-interp (cadr E) P)
-  )
-)
-
-; Helper function which computes the primitive rest function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-rest (E P)
-  (cdr
-    (fl-interp (cadr E) P)
-  )
-)
-
-; Helper function which computes the primitive cons function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-cons (E P)
-  (cons (fl-interp (cadr E) P) (fl-interp (caddr E) P))
-)
-
-; Helper function which computes the primitive equal function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-equal (E P)
-  (equal
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive number function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-number (E P)
-  (numberp (fl-interp (cadr E) P))
-)
-
-; Helper function which computes the primitive addition function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-add (E P)
-  (+
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive subtraction function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-sub (E P)
-  (-
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive multiplication function.
-;
-; Test Cases:
-; ...
-(defun fl-interp-mul (E P)
-  (*
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive greater-than function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-greater-than (E P)
-  (>
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive less-than function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-less-than (E P)
-  (<
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive equivalence function.
-;
-; Test Cases:
-; ...
-(defun fl-interp-equals (E P)
-  (=
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive and function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-and (E P)
-  (and
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive or function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-or (E P)
-  (or
-    (fl-interp (cadr E) P)
-    (fl-interp (caddr E) P)
-  )
-)
-
-; Helper function which computes the primitive not function.
-;
-; Test Cases:
-;
-; ...
-(defun fl-interp-not (E P)
-  (not
-    (fl-interp (cadr E) P)
   )
 )
 
